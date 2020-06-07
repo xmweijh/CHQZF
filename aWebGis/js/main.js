8471d883d662b7d385c69d464d1d0ca5963b0894
xmweijh <xmweijh@foxmail.com>
2020-06-07 22:08:24 +0800

    2nd

--- aWebGis/js/main.js
+++ aWebGis/js/main.js
@@ -1,24 +1,20 @@
-var map;
-var dialog,toolbar, symbol, geomTask;;
+var map, tb;
+var dialog;
 //dojo.registerModulePath('js',location.pathname.replace(/\/[^/]*$/, '') + '/js');
 require([
-    "esri/map", "esri/layers/ArcGISTiledMapServiceLayer", "esri/layers/FeatureLayer",
+    "esri/map", "esri/layers/ArcGISTiledMapServiceLayer", "esri/layers/FeatureLayer","esri/layers/GraphicsLayer",
     "dojo/dom", "esri/dijit/OverviewMap",
-    "esri/symbols/SimpleFillSymbol", "esri/symbols/SimpleMarkerSymbol","esri/symbols/SimpleLineSymbol",
+    "esri/symbols/SimpleFillSymbol", "esri/symbols/SimpleMarkerSymbol","esri/symbols/PictureFillSymbol","esri/symbols/SimpleLineSymbol",
     "esri/symbols/PictureMarkerSymbol","esri/renderers/SimpleRenderer", "esri/graphic", "esri/lang",
     "esri/Color", "dojo/number", "dojo/dom-style",
     "dijit/TooltipDialog", "dijit/popup",
-    "esri/toolbars/draw",
-    "dojo/parser", "dijit/registry",
-    "dijit/layout/BorderContainer", "dijit/layout/ContentPane",
-    "dijit/form/Button", "dijit/WidgetSet",
+    "esri/toolbars/draw","esri/symbols/CartographicLineSymbol","dojo/on", "esri/dijit/HomeButton",
     "dojo/domReady!"
-    ], function(Map, ArcGISTiledMapServiceLayer, FeatureLayer,dom, OverviewMap,SimpleFillSymbol, SimpleMarkerSymbol,
+    ], function(Map, ArcGISTiledMapServiceLayer, FeatureLayer,GraphicsLayer,dom, OverviewMap,SimpleFillSymbol, SimpleMarkerSymbol, PictureFillSymbol,
                 SimpleLineSymbol,PictureMarkerSymbol,SimpleRenderer, Graphic, esriLang,
                 Color, number, domStyle,
-                TooltipDialog, dijitPopup,Draw,
-                parser, registry){
-    parser.parse();
+                TooltipDialog, dijitPopup,Draw,CartographicLineSymbol,on,HomeButton
+                ){
 
     map = new Map("mapDiv",{
     logo:false,
@@ -98,45 +94,78 @@ require([
     }
 
 
-    map.on("load", createToolbar);
-    // loop through all dijits, connect onClick event
-    // listeners for buttons to activate drawing tools
-    registry.forEach(function(d) {
-        // d is a reference to a dijit
-        // could be a layout container or a button
-        if ( d.declaredClass === "dijit.form.Button" ) {
-            d.on("click", activateTool);
-        }
-    });
+    var graphicsLayer = new GraphicsLayer();
+    map.addLayer(graphicsLayer);
+    map.on("load", initToolbar);
 
-    function activateTool() {
-        var tool = this.label.toUpperCase().replace(/ /g, "_");
-        toolbar.activate(Draw[tool]);
-        map.hideZoomSlider();
+    // markerSymbol is used for point and multipoint, see http://raphaeljs.com/icons/#talkq for more examples
+    var markerSymbol = new SimpleMarkerSymbol();
+    markerSymbol.setPath("M16,4.938c-7.732,0-14,4.701-14,10.5c0,1.981,0.741,3.833,2.016,5.414L2,25.272l5.613-1.44c2.339,1.316,5.237,2.106,8.387,2.106c7.732,0,14-4.701,14-10.5S23.732,4.938,16,4.938zM16.868,21.375h-1.969v-1.889h1.969V21.375zM16.772,18.094h-1.777l-0.176-8.083h2.113L16.772,18.094z");
+    markerSymbol.setColor(new Color("#00FFFF"));
+
+    // lineSymbol used for freehand polyline, polyline and line.
+    var lineSymbol = new CartographicLineSymbol(
+        CartographicLineSymbol.STYLE_SOLID,
+        new Color([255,0,0]), 10,
+        CartographicLineSymbol.CAP_ROUND,
+        CartographicLineSymbol.JOIN_MITER, 5
+    );
+
+    // fill symbol used for extent, polygon and freehand polygon, use a picture fill symbol
+    // the images folder contains additional fill images, other options: sand.png, swamp.png or stiple.png
+    var fillSymbol = new PictureFillSymbol(
+        "images/done.png",
+        new SimpleLineSymbol(
+            SimpleLineSymbol.STYLE_SOLID,
+            new Color('#000'),
+            1
+        ),
+        42,
+        42
+    );
+
+    function initToolbar() {
+        tb = new Draw(map);
+        tb.on("draw-end", addGraphic);
+
+        // event delegation so a click handler is not
+        // needed for each individual button
+        on(dom.byId("info"), "click", function(evt) {
+            if ( evt.target.id === "info" ) {
+                return;
+            } else if (evt.target.id ==="Delete"){
+                graphicsLayer.clear();
+                tb.activate(tool);
+                return;
+            }
+            var tool = evt.target.id.toLowerCase();
+            map.disableMapNavigation();
+            tb.activate(tool);
+        });
     }
 
-    function createToolbar(themap) {
-        toolbar = new Draw(map);
-        toolbar.on("draw-end", addToMap);
-    }
+    function addGraphic(evt) {
+        //deactivate the toolbar and clear existing graphics
+        tb.deactivate();
+        map.enableMapNavigation();
 
-    function addToMap(evt) {
+        // figure out which symbol to use
         var symbol;
-        toolbar.deactivate();
-        map.showZoomSlider();
-        switch (evt.geometry.type) {
-            case "point":
-            case "multipoint":
-                symbol = new SimpleMarkerSymbol();
-                break;
-            case "polyline":
-                symbol = new SimpleLineSymbol();
-                break;
-            default:
-                symbol = new SimpleFillSymbol();
-                break;
+        if ( evt.geometry.type === "point" || evt.geometry.type === "multipoint") {
+            symbol = markerSymbol;
+        } else if ( evt.geometry.type === "line" || evt.geometry.type === "polyline") {
+            symbol = lineSymbol;
         }
-        var graphic = new Graphic(evt.geometry, symbol);
-        map.graphics.add(graphic);
+        else {
+            symbol = fillSymbol;
+        }
+        graphicsLayer.add(new Graphic(evt.geometry, symbol));
+        // map.graphics.add(new Graphic(evt.geometry, symbol));
     }
+
+    var home = new HomeButton({
+        map: map
+    }, "HomeButton");
+    home.startup();
+
 });
